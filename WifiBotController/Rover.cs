002ef246f3ddb602e6939bad8ad5d64ee67e08ef
Eric STOCLET <eric.stoclet@edmond-labbe.fr>
2019-02-15 10:25:42 +0100

    Maj 15/02/2019 10:25

--- WifiBotController/Rover.cs
+++ WifiBotController/Rover.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Networking;
@@ -25,15 +26,34 @@ namespace WifiBotController
         public Rover(string host, string port)
         {
             // Instancier SocketClient
+            TcpClient SocketClient = new TcpClient();
+            NetworkStream Netstream;
 
             // Initialiser connexionOk à false
+            connexionOK = false;
 
             // Initialiser les attributs adresseServeur et port avec les paramètres reçus
+            adresseServeur = host;
+            this.port = port;
 
             // Initialiser l’attribut commandeAEnvoyer à [0,0]
+            commandeAEnvoyer = [ 0, 0 ];
         }
 
-        public void connexion() { }
+        public void connexion()
+        {
+            // Connexion
+            try
+            {
+                socketClient.Connect(host, port);
+                Netstream = SocketClient.GetStream();
+
+            }
+            catch (SocketException e)
+            {
+                string msgErreur = "Erreur à la connexion : " + e;
+            }
+        }
 
         public void deconnexion() { }
 
